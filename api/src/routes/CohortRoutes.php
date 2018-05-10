cf95e88dc508074163eecddc686f3be1b8a187e6
Alejandro Sanchez <aalejo@gmail.com>
2018-05-10 19:58:48 +0000

    updated services for put

--- api/src/routes/CohortRoutes.php
+++ api/src/routes/CohortRoutes.php
@@ -17,7 +17,7 @@ class CohortRoutes{
         $app->get('/students/cohort/{cohort_id}', array($cohortHandler, 'getCohortStudentsHandler'))->add($scopes(['super_admin']));
         
         $app->post('/student/cohort/{cohort_id}', array($cohortHandler, 'addStudentToCohortHandler'))->add($scopes(['super_admin']));
-        $app->post('/cohort/', array($cohortHandler, 'createCohortHandler'))->add($scopes(['super_admin']));
+        $app->put('/cohort/', array($cohortHandler, 'createCohortHandler'))->add($scopes(['super_admin']));
         $app->post('/cohort/{cohort_id}', array($cohortHandler, 'updateCohortHandler'))->add($scopes(['super_admin']));
         $app->delete('/cohort/{cohort_id}', array($cohortHandler, 'deleteCohortHandler'))->add($scopes(['super_admin']));
         
--- api/src/routes/LocationRoutes.php
+++ api/src/routes/LocationRoutes.php
@@ -13,7 +13,7 @@ class LocationRoutes{
         $app->get('/locations/', array($locationHandler, 'getAllHandler'));//->add($scopes(['read_basic_info']));
         $app->get('/location/{location_id}', array($locationHandler, 'getSingleHandler'))->add($scopes(['read_basic_info']));
         
-        $app->post('/location/', array($locationHandler, 'createLocationHandler'))->add($scopes(['super_admin']));
+        $app->put('/location/', array($locationHandler, 'createLocationHandler'))->add($scopes(['super_admin']));
         $app->post('/location/{location_id}', array($locationHandler, 'updateLocationHandler'))->add($scopes(['super_admin']));
         $app->delete('/location/{location_id}', array($locationHandler, 'deleteLocationHandler'))->add($scopes(['super_admin']));
         
